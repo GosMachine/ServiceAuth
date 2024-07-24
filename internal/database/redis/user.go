609970e664466a64c53a63d5b85ec847e55c5b2a
gosmach1ne <1234567yhb@gmail.com>
2024-07-24 16:57:14 +0300

    optimized

--- internal/database/redis/user.go
+++ internal/database/redis/user.go
@@ -28,3 +28,32 @@ func (r *Redis) CreateToken(email string, expiration time.Duration) string {
 func (r *Redis) DeleteToken(token string) error {
 	return r.Client.Del(context.Background(), token).Err()
 }
+
+func (r *Redis) SetEmailVerifiedCache(email string, value bool) error {
+	r.Client.Set(r.Ctx, "emailVerified:"+email, value, time.Hour*24)
+	return nil
+}
+
+func (r *Redis) GetEmailVerifiedCache(email string) (bool, error) {
+	verified, err := r.Client.Get(r.Ctx, "emailVerified:"+email).Bool()
+	if err != nil {
+		r.Log.Error("error get user data from cache", zap.Error(err))
+		verified, err = r.AuthClient.EmailVerified(context.Background(), email)
+		r.SetEmailVerifiedCache(email, verified)
+		return verified, err
+	}
+	r.Log.Info("emailVerified from cache", zap.String("email", email))
+	return verified, nil
+}
+
+func (r *Redis) GetToken(token string) string {
+	return r.Client.Get(context.Background(), token).Val()
+}
+
+func (r *Redis) DeleteToken(token string) error {
+	return r.Client.Del(context.Background(), token).Err()
+}
+
+func (r *Redis) GetTokenTTL(token string) time.Duration {
+	return r.Client.TTL(context.Background(), token).Val()
+}
